04b778fb4ad227bc1adc7cdbd5155c819b059ef0
CRAMSUS <powercode302@gmail.com>
2023-05-12 14:38:07 +0800

    update

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,4 +1,10 @@
 
+// window.addEventListener('load', function () {
+//     document.querySelector('body').classList.add("loaded");
+// });
+
 window.addEventListener('load', function () {
+    console.log('Page loaded');
     document.querySelector('body').classList.add("loaded");
-});
+    console.log('Loaded class added');
+  });
