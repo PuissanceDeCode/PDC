d6599f9645da63116a9381e3f2be2f47277a9351
PuissanceDeCode <133269083+PuissanceDeCode@users.noreply.github.com>
2023-05-12 14:48:26 +0800

    Update preloader.js

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,10 +1,3 @@
-
-// window.addEventListener('load', function () {
-//     document.querySelector('body').classList.add("loaded");
-// });
-
 window.addEventListener('load', function () {
-    console.log('Page loaded');
     document.querySelector('body').classList.add("loaded");
-    console.log('Loaded class added');
   });
